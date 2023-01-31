81d8b6ededdada7b1e8449a82dd10bd8a34bf241
Shawn Johnson <johnson.sg@gmail.com>
2023-01-31 15:01:21 -0500

    send post request with new fetch object

--- src/Movies_Post.js
+++ src/Movies_Post.js
@@ -15,23 +15,40 @@ function App() {
     // If you are not using asynce/await, then .then.catch promise should be used.
     // If using Axios,it would throw an error status, so you wouldn't have to use try/catch
     try {
-      const response = await fetch(`https://swapi.dev/api/films/`)
+      // const response = await fetch(`https://swapi.dev/api/films/`)
+      const response = await fetch(
+        `https://react-movies-bd4ff-default-rtdb.firebaseio.com/movies.json`
+      )
       // Catch error and error message condition
       if (!response.ok) {
         throw new Error('Something went wrong :(') // the string is the 'message'
       }
 
       const data = await response.json()
+      // console.log('FetchData', data)
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies) // If not restructuring use: data.results
+      // When the data we get back (fetch) is an Object we must transform it to an array
+      const loadedMovies = []
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+      setMovies(loadedMovies)
+
+      // When the data we get back (fetch) is an array
+      // const transformedMovies = data.results.map(movieData => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date
+      //   }
+      // })
+      // setMovies(transformedMovies) // If not restructuring use: data.results
     } catch (error) {
       setError(error.message)
     }
@@ -43,9 +60,21 @@ function App() {
     fetchMoviesHandler()
   }, [fetchMoviesHandler])
 
-  // Add Movie
-  function addMovieHandler(movie) {
-    console.log(movie)
+  // Add Movie handler
+  // Note: you can also add Try/Catch to this as well, Recommended.
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      `https://react-movies-bd4ff-default-rtdb.firebaseio.com/movies.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    const data = await response.json()
+    // console.log('PostData', data)
   }
 
   // Clean up inline content
